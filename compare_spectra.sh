fcf673e5dde83d2bfbb5c4ffdd3ae65d94e0c751
Jesse Becker <hawson@gmail.com>
2020-01-25 19:10:06 -0500

    Comments for convert_spectra

--- compare_spectra.sh
+++ compare_spectra.sh
@@ -4,17 +4,36 @@ F=${1:-sample-20200125-170527-B16R8000-timestamped.wav}
 L=low.wav
 N=nreduct.wav
 
+RATE=2000
+
 SSTART=15
 SEND=18
 NPROF=${F/wav/profile}
 
+echo Source: $F
+echo LowPass: $L
+echo NR: $N
 
-
-sox $F -n  spectrogram -o orig.png 
+echo "Profiling noise from $SSTART to $SEND"
 sox $F -n  noiseprof ${NPROF}
 
-sox $F --rate 2000 $L sinc -1k
-sox $L -n spectrogram -o ${L/wav/png}
+echo "Low pass filter @ $RATE Hz"
+sox $F --rate $RATE $L sinc -1k
 
+echo "Noise reduction..."
 sox $F $N noisered ${NPROF} 0.1 
-sox $N -n spectrogram -o nreduct.png
+
+echo "Noise reduction on LP output"
+sox $N LP-$N noisered ${NPROF} 0.1 
+
+echo "Spectrograms..."
+sox $F    -n spectrogram -o orig.png 
+sox $L    -n spectrogram -o ${L/wav/png}
+sox $N    -n spectrogram -o nreduct.png
+sox LP-$N -n spectrogram -o LP-nreduct.png
+
+for file in $F $L $N LP-$N; do
+	echo "parsing/dumping $file"
+	./parse.py $file | tail -n +6 | nl > ${file/wav/dat}
+done
+
